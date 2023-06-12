599114b6b1f44e2f275d0a07a0a26e4d97390bf4
scottbigbrain <irrationallogic42@gmail.com>
2023-06-12 12:28:39 -0600

    More refactoring. Can't make up my mind on where stuff should go lol

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,11 @@
-use std::time::SystemTime;
+use std::{ time::SystemTime, error::Error };
 use time::OffsetDateTime;
 use nostr_sdk::EventId;
 use clap::{ Parser, Subcommand };
+use dialoguer::{ Confirm, Input, Editor };
+use confy;
+use nostr_sdk::prelude::*;
+use crate::config::Config;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -29,4 +33,51 @@ pub fn print_check_log(feed_url: &String) {
 pub fn print_notify_log(event_id: EventId) {
     let notifcation_timestamp: OffsetDateTime = SystemTime::now().into();
     println!("Broadcasted new episode notifcation at {notifcation_timestamp} with {event_id}")
+}
+
+pub fn generate_keys(mut cfg: Config) -> Result<(), Box<dyn Error>> {
+    let keys = Keys::generate();
+    let public_key = keys.public_key().to_bech32().unwrap();
+    let secret_key = keys.secret_key().unwrap().to_bech32().unwrap();
+    println!("Keys Generated\nPublic Key: {public_key}\nPrivate Key: {secret_key}");
+    
+    let store_keys = Confirm::new()
+        .with_prompt("Do you want to store the new keys to config? This will overwrite the currently stored key.")
+        .interact()?;
+    
+    if store_keys {
+        cfg.secret_key = secret_key;
+        cfg.public_key = public_key;
+        confy::store("nostrnotify", None, cfg)?;
+        println!("Keys stored to {}", confy::get_configuration_file_path("nostrnotify", None).unwrap().to_str().unwrap());
+    } else {
+        println!("Gotcha. Keys not stored");
+    }
+    
+    Ok(())
+}
+
+pub fn request_user_metadata(mut cfg: Config) -> Result<(), Box<dyn Error>> {
+    let username: String = Input::new()
+        .with_prompt("Enter bot username")
+        .interact_text()?;
+    cfg.name = username;
+    let display_name: String = Input::new()
+        .with_prompt("Enter bot display name")
+        .interact_text()?;
+    cfg.display_name = display_name;
+        
+    let include_description = Confirm::new()
+        .with_prompt("Do you want to include a description?")
+        .interact()?;
+    
+    let description = if include_description {
+        Editor::new().edit("Enter bot description").unwrap().unwrap()
+    } else {
+        String::new()
+    };
+    cfg.description = description;
+    
+    confy::store("nostrnotify", None, cfg)?;
+    Ok(())
 }
